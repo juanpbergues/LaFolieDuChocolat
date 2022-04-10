e173e74887a6381f17a9dfdce0488036956fe8ab
juanpbergues <32652663+juanpbergues@users.noreply.github.com>
2022-04-09 21:55:26 -0300

    Adding libraries and eslint configuration

--- la-folie-du-chocolat/src/App.js
+++ la-folie-du-chocolat/src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './app/styles/app.scss'
+import {
+  AppBar,
+  Container,
+  Toolbar,
+  Typography,
+  Box,
+  Button
+} from '@mui/material'
+import logo from './assets/images/logo.png'
 
-function App() {
+const App = () => {
+  const pages = ['Products', 'Pricing', 'Blog']
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <AppBar position="static" color="inherit">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          { /* Si el tamaño de la pantalla es grande, se renderiza el logo y los botones */ }
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ mr: 2, display: { xs: 'none', md: 'flex' } }}
+          >
+            <img src={logo} height='100vh' />
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            {pages.map((page) => (
+              <Button
+                key={page}
+                sx={{ my: 2, color: 'black', display: 'block' }}
+              >
+                {page}
+              </Button>
+            ))}
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  )
 }
 
-export default App;
+export default App
